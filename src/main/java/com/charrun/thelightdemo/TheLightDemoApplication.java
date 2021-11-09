08639f607c5299d0ab64252cfc3e5fa344dd2179
CharZhou <zhouzewei@fjtjg.com>
2021-11-09 16:25:53 +0800

    first commit

---
+++ src/main/java/com/charrun/thelightdemo/TheLightDemoApplication.java
@@ -0,0 +1,16 @@
+package com.charrun.thelightdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hacket
+ */
+@SpringBootApplication
+public class TheLightDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheLightDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/charrun/thelightdemo/ThelightdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charrun.thelightdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThelightdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
